2939cf894f440a37d94c309f23f467b40030b5d0
hanolee <hano.dev.kr@gmail.com>
2020-12-16 08:15:33 +0900

    First Vanilla On Github

---
+++ Simple Viewer - BlockChain/script.js
@@ -0,0 +1,133 @@
+
+
+EndorfinOracleContractABI = [
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [],
+		"name": "getLatestBTC",
+		"outputs": [
+			{
+				"internalType": "int256",
+				"name": "",
+				"type": "int256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getLatestBTCDate",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getLatestDAI",
+		"outputs": [
+			{
+				"internalType": "int256",
+				"name": "",
+				"type": "int256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getLatestDAIDate",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getLatestETH",
+		"outputs": [
+			{
+				"internalType": "int256",
+				"name": "",
+				"type": "int256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getLatestETHDate",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getLatestXRP",
+		"outputs": [
+			{
+				"internalType": "int256",
+				"name": "",
+				"type": "int256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getLatestXRPDate",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+EndorfinOracleContractAddress = "0xd3e62E97E339DC3E3dCB12C7ACFDd043066635BA"
+
+const web3 = new Web3("https://kovan.infura.io/v3/aa747461ed3544b9b8ba0eb895571eec");
+
+const addr = "0x9326BFA02ADD2366b30bacB125260Af641031331";
+const priceFeed = new web3.eth.Contract(aggregatorV3InterfaceABI, addr);
+priceFeed.methods.latestRoundData().call()
+    .then((roundData) => {
+        // Do something with roundData
+        console.log("Latest Round Data", roundData)
+    });
+
+function showETHUSD() {
+
+    
+
+    
+
+    
+}
